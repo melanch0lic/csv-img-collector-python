6b67bd7b93f93f3010ea1ceaa81b1a19f4adac74
Zaur Kudzaev <kudza76@mail.ru>
2021-07-09 13:16:04 +0300

    imported selenium, added windows geckodriver for firefox browser, set browser settings in the _get_browser function, implemented loading images from britannica, pexels, ccsearch

--- imgcollector.py
+++ imgcollector.py
@@ -4,6 +4,8 @@ import csv
 import os.path
 from soupsieve import select_one
 import time
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 results = []
 
@@ -40,31 +42,97 @@ def upload_wiki(key_word, column_name):
             file_dir, "wb",
         ) as f:
             f.write(photo)
-    os.chdir("..")
 
     time.sleep(2)
+
+def _get_browser():
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference('browser.chrome.favicons', False)
+    fp.set_preference('browser.chrome.site_icons', False)
+    print(os.getcwd()) 
+    geckodriver_path = os.path.join(
+        os.path.expanduser('~'),'csv-img-collector-python\\geckodriver.exe')
+    if not os.path.exists(geckodriver_path):
+        raise RuntimeError(
+            'Improperly configured. Expecting geckodriver executable in {}'.format(geckodriver_path))
+    browser = webdriver.Firefox(
+        executable_path=geckodriver_path,
+        firefox_profile=fp)
+    browser.set_window_size(1224, 768)
+    return browser
 
 
 def upload_britannica(key_word, column_name):
     base_url = "https://www.britannica.com/search?query="
-    html = requests.get(base_url + key_word).text
-    soup = BeautifulSoup(html, "html.parser")
-    img = soup.select("img")
-    print(img)
-    img_src = img.attrs["src"]
+    browser = _get_browser()
+    browser.get(base_url + key_word)
+    img = browser.find_element_by_css_selector('.SearchFeature .grid.p-20 img')
 
-    time.sleep(2)
-
+    image_url = img.get_attribute('src')
     headers = {
         "User-Agent": "Mozilla/5.0(Windows NT 10.0WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 YaBrowser/19.10.2.195 Yowser/2.5 Safari/537.36"
     }
     response = requests.get(
-        "https:" + img_src, headers=headers)
+        image_url, headers=headers)
     photo = response.content
     
     if not os.path.exists("images"):
-        file_dir = os.path.join('britannica.' + img_src.attrs["href"][-3] + img_src.attrs["href"][-2] + img_src.attrs["href"][-1])
-    else: file_dir = os.path.join(column_name, key_word,'britannica.' + img_src.attrs["href"][-3] + img_src.attrs["href"][-2] + img_src.attrs["href"][-1])
+        file_dir = os.path.join('britannica.' + image_url[-3] + image_url[-2] + image_url[-1])
+    else: file_dir = os.path.join(column_name, key_word,'britannica.' + image_url[-3] + image_url[-2] + image_url[-1])
+
+    if response.status_code == 200:
+        with open(
+            file_dir, "wb",
+        ) as f:
+            f.write(photo)
+
+    time.sleep(2)
+
+
+def upload_pexels(key_word, column_name):
+    base_url = "https://www.pexels.com/ru-ru/search/"
+    browser = _get_browser()
+    browser.get(base_url + key_word)
+    img = browser.find_element_by_css_selector('img.photo-item__img:first-child')
+
+    image_url = img.get_attribute('src')
+    headers = {
+        "User-Agent": "Mozilla/5.0(Windows NT 10.0WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 YaBrowser/19.10.2.195 Yowser/2.5 Safari/537.36"
+    }
+    response = requests.get(
+        image_url, headers=headers)
+    photo = response.content
+    
+    if not os.path.exists("images"):
+        file_dir = 'pexels.jpg'
+    else: file_dir = os.path.join(column_name, key_word,'pexels.jpg')
+
+    if response.status_code == 200:
+        with open(
+            file_dir, "wb",
+        ) as f:
+            f.write(photo)
+
+    time.sleep(2)
+
+
+def upload_ccsearch(key_word, column_name):
+    base_url = "https://search.creativecommons.org/search?q="
+    browser = _get_browser()
+    browser.get(base_url + key_word)
+    img = browser.find_element_by_css_selector('img.search-grid_image:first-child')
+
+    image_url = img.get_attribute('src')
+    headers = {
+        "User-Agent": "Mozilla/5.0(Windows NT 10.0WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 YaBrowser/19.10.2.195 Yowser/2.5 Safari/537.36"
+    }
+    response = requests.get(
+        image_url, headers=headers)
+    photo = response.content
+    
+    if not os.path.exists("images"):
+        file_dir = 'ccsearch.jpg'
+    else: file_dir = os.path.join(column_name, key_word,'ccsearch.jpg')
 
     if response.status_code == 200:
         with open(
@@ -74,6 +142,7 @@ def upload_britannica(key_word, column_name):
     os.chdir("..")
 
     time.sleep(2)
+
 
 def readCSV(csvfile_name, column_name):
     with open(csvfile_name) as File:
@@ -91,14 +160,17 @@ def readCSV(csvfile_name, column_name):
                 os.makedirs(i[column_name])
                 os.chdir(i[column_name])   
                 upload_wiki(i[column_name], column_name)
-                upload_britannica(i[column_name], column_name) 
-            
+                upload_britannica(i[column_name], column_name)
+                upload_pexels(i[column_name], column_name) 
+                upload_ccsearch(i[column_name], column_name)
             elif os.path.exists(i[column_name]):
                 print(os.getcwd())  
-                if len(os.listdir(i[column_name]))<2:
+                if len(os.listdir(i[column_name]))<5:
                     os.chdir(i[column_name]) 
                     upload_wiki(i[column_name], column_name)  
-                    upload_britannica(i[column_name], column_name) 
+                    upload_britannica(i[column_name], column_name)
+                    upload_pexels(i[column_name], column_name)
+                    upload_ccsearch(i[column_name], column_name)  
 
     else:
         col_name = list(results[0].keys())[0]
@@ -114,13 +186,17 @@ def readCSV(csvfile_name, column_name):
                 os.chdir(cell_name)   
                 upload_wiki(cell_name, col_name)
                 upload_britannica(cell_name, col_name)
+                upload_pexels(cell_name, col_name)
+                upload_ccsearch(cell_name, col_name) 
 
             elif os.path.exists(cell_name):
                 print(os.getcwd())  
-                if len(os.listdir(cell_name))<1:
+                if len(os.listdir(cell_name))<5:
                     os.chdir(cell_name) 
                     upload_wiki(cell_name, col_name) 
-                    upload_britannica(cell_name, col_name)  
+                    upload_britannica(cell_name, col_name)
+                    upload_pexels(cell_name, col_name)
+                    upload_ccsearch(cell_name, col_name)    
 
 
 def main():
